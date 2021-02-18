0b392ed9fdfc575118ef04eb7f99c2a43ede76ea
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-18 20:05:57 +0000

    adding withRouter as higher compoenent to touter

--- e-clothing/src/App.js
+++ e-clothing/src/App.js
@@ -1,11 +1,22 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Homepage from './Pages/Homepage.component/HomePage'
 
+const hatPage = () => {
+  return <div><h1>Hello</h1></div>
+}
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
-    </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route component={hatPage} />
+        </Switch>
+      </Router>
+    </div >
   );
 }
 
