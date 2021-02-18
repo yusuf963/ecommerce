12ab970292266493b6358e93a1e3fb4ce305c5c4
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-18 21:46:08 +0000

    creating header componenet and link to home pageand styling

--- e-clothing/src/App.js
+++ e-clothing/src/App.js
@@ -3,12 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
 import Homepage from './Pages/Homepage.component/HomePage'
 import ShopPage from './Pages/shop/shop.compoenent'
+import Header from './Components/header/header-component'
 
 
 function App() {
   return (
-    <div className="App">
+    <div>
+
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path='/shop' component={ShopPage} />
