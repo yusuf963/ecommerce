95c21aee23b4cebfe308b56d14ea3fab0332f467
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-23 22:08:04 +0000

    creating sign out

--- e-clothing/src/Components/header/header-component.js
+++ e-clothing/src/Components/header/header-component.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { auth } from '../../firebase/firebase.utility'
 import { ReactComponent as Logo } from '../../asset/crown.svg'
 import './header-style.scss'
 
-const Header = () => {
+const Header = ({ currentUser }) => {
   return <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -11,6 +12,11 @@ const Header = () => {
     <div className='options'>
       <Link className='option' to='/shop'>SHOP</Link>
       <Link className='option' to='/contact'>CONTACT</Link>
+      {currentUser ?
+        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+        :
+        <Link className='option' to='/signin'>SIGN IN</Link>
+      }
     </div>
   </div>
 }
